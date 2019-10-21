90ce96b892d4e280dd0fe4fef935b63b6fd9166e
nevace <liam.neville89@gmail.com>
2019-10-21 19:59:52 +0100

    holding page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,25 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import SEO from '../components/seo';
+import styled from 'styled-components';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = () => {
+	return (
+		<IndexStyled>
+			<h1>noak & co</h1>
+			{/*<SEO title="Home" />*/}
+		</IndexStyled>
+	);
+};
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export const IndexStyled = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	width: 100%;
+	background-color: #02b69c;
+	font-size: 30px;
+	color: #fff;
+`;
 
-export default IndexPage
+export default IndexPage;
