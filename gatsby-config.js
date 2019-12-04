32b4662324081b2f1a5e1cde8da52d444da1c412
Ben Oakenfull <benoakenfull@gmail.com>
2019-12-04 17:34:07 +0000

    Meta data update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		title: `noak & co | Helping companies to deliver leading products`,
+    	description: `Challenger consultancy, helping companies deliver products whilst up skilling employees to provide long term value.`,
 		author: `@gatsbyjs`
 	},
 	plugins: [
@@ -24,7 +24,7 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+				icon: `src/images/favicon.png` // This path is relative to the root of the site.
 			}
 		},
 		{
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import SEO from '../components/seo';
 import styled from 'styled-components';
+import { Helmet } from "react-helmet"
 
 const IndexPage = () => {
 	return (
 		<IndexStyled>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>noak &amp; co | Helping companies to deliver leading products</title>
+				<description>Challenger consultancy, helping companies deliver products whilst up skilling employees to provide long term value.</description>
+				<link rel="canonical" href="http://mysite.com/example" />
+			</Helmet>
 			<h1>noak & co</h1>
 			{/*<SEO title="Home" />*/}
 		</IndexStyled>
